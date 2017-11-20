ad327a20c3f77813e3d4c290c2ec5f4a4d95ae56
Dario Mosca <mosca.dario@gmail.com>
2017-11-20 23:23:44 +0100

    addIndexInfos

--- app/src/main/java/dario/mosca/footstats/business.java
+++ app/src/main/java/dario/mosca/footstats/business.java
@@ -8,15 +8,39 @@ import org.jetbrains.annotations.Contract;
 
 public class business {
 
-    @Contract(pure = true)
+    @org.jetbrains.annotations.Contract(pure = true)
     public static Integer calculateMatchs(){
         Integer res = 3+2;
         return res;
     }
 
-    @org.jetbrains.annotations.Contract(pure = true)
+    @Contract(pure = true)
     public static Integer calculateTime(){
         Integer res = 90 + 45 + 30 + 60;
+        return res;
+    }
+
+    @Contract(pure = true)
+    public static Integer goals(){
+        Integer res = 1 + 0 + 2 + 0;
+        return res;
+    }
+
+    @Contract(pure = true)
+    public static Integer passes(){
+        Integer res = 2 + 1 + 1 + 0;
+        return res;
+    }
+
+    @Contract(pure = true)
+    public static Integer yellowCards(){
+        Integer res = 0;
+        return res;
+    }
+
+    @Contract(pure = true)
+    public static Integer redCards(){
+        Integer res = 0;
         return res;
     }
 }
--- app/src/main/java/dario/mosca/footstats/main.java
+++ app/src/main/java/dario/mosca/footstats/main.java
@@ -11,6 +11,12 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class main extends AppCompatActivity {
+    final String NB_MATCHS = "Séléction(s) : ";
+    final String GAME_TIME = "Temps de jeu : ";
+    final String GOALS = "But(s) marqué(s) : ";
+    final String PASSES = "Passe(s) de but : ";
+    final String YELLOW_CARDS = "Carton(s) jaune(s) : ";
+    final String RED_CARDS = "Carton(s) rouge(s) : ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,28 @@ public class main extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //nbMatchs
-        TextView nbMatchs = (TextView) findViewById(R.id.time);
-        nbMatchs.setText("Nombre de matchs joués : " + business.calculateMatchs());
+        TextView nbMatchs = (TextView) findViewById(R.id.matchs);
+        nbMatchs.setText(NB_MATCHS + business.calculateMatchs());
 
         //gameTime
-        TextView gameTime = (TextView) findViewById(R.id.matchs);
-        gameTime.setText("Temps de jeu : " + business.calculateTime() + " min.");
+        TextView gameTime = (TextView) findViewById(R.id.time);
+        gameTime.setText(GAME_TIME + business.calculateTime() + " min.");
+
+        //goals
+        TextView goals = (TextView) findViewById(R.id.goals);
+        goals.setText(GOALS + business.goals());
+
+        //passes
+        TextView passes = (TextView) findViewById(R.id.passes);
+        passes.setText(PASSES + business.passes());
+
+        //yellowCards
+        TextView yellow = (TextView) findViewById(R.id.yellow);
+        yellow.setText(YELLOW_CARDS + business.yellowCards());
+
+        //redCards
+        TextView red = (TextView) findViewById(R.id.red);
+        red.setText(RED_CARDS + business.redCards());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
