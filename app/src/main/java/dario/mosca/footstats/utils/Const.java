240aff1d9943db9b143aa1eb644ec35646f4db64
Daniel Chappuis <daniel.chappuis.suisse@gmail.com>
2017-12-22 22:12:01 +0100

    Add Fragment
    Move final variables into static const class,
    add fragment for both current screens

---
+++ app/src/main/java/dario/mosca/footstats/utils/Const.java
@@ -0,0 +1,14 @@
+package dario.mosca.footstats.utils;
+
+/**
+ * Created by d-a-d on 22.12.2017.
+ */
+
+public class Const {
+    public static final String NB_MATCHS = "Séléction(s) : ";
+    public static final String GAME_TIME = "Temps de jeu : ";
+    public static final String GOALS = "But(s) marqué(s) : ";
+    public static final String PASSES = "Passe(s) de but : ";
+    public static final String YELLOW_CARDS = "Carton(s) jaune(s) : ";
+    public static final String RED_CARDS = "Carton(s) rouge(s) : ";
+}
